ddefd9de709076fdc22dea583901d5919f04e3a3
Henry Lau <henrylau9677gmail.com>
2019-05-27 01:13:26 +0800

    added effects for project component

--- src/components/project/project.js
+++ src/components/project/project.js
@@ -4,15 +4,14 @@ import './project.css'
 
 const project = (props) => {
     return (
-        <div className="project">
-            <a href={props.projectLink} target="_blank">
-                <img className="image" src={props.projectImage} />
+        <a href={props.projectLink} target="_blank" className={"project " + props.projectClassName}>
+            <div className="inner-div"  style={{ backgroundImage: "url("+props.projectImage+")" }}>
                 <span className="title">{props.projectTitle}</span>
-                <div className="desc">
-                    <span>{props.projectDesc}</span>
-                </div>
-            </a>
-        </div>
+            </div>
+            <div className="desc">
+                <span>{props.projectDesc}</span>
+            </div>
+        </a>
     )
 }
 
--- src/sections/projects/projects.js
+++ src/sections/projects/projects.js
@@ -11,31 +11,31 @@ class Projects extends Component {
             projects: [
                 {
                     projectTitle: "China Mobile International (CMI)",
-                    projectLink: "www.youtube.com",
+                    projectLink: "http://www.youtube.com",
                     projectDesc: "Ad cillum cupidatat dolore non nisi nisi.",
                     projectImage: "/images/testing.png"
                 },
                 {
                     projectTitle: "China Mobile International (CMI)",
-                    projectLink: "www.youtube.com",
+                    projectLink: "http://www.youtube.com",
                     projectDesc: "Ad cillum cupidatat dolore non nisi nisi.",
                     projectImage: "/images/testing.png"
                 },
                 {
                     projectTitle: "China Mobile International (CMI)",
-                    projectLink: "www.youtube.com",
+                    projectLink: "http://www.youtube.com",
                     projectDesc: "Ad cillum cupidatat dolore non nisi nisi.",
                     projectImage: "/images/testing.png"
                 },
                 {
                     projectTitle: "China Mobile International (CMI)",
-                    projectLink: "www.youtube.com",
+                    projectLink: "http://www.youtube.com",
                     projectDesc: "Ad cillum cupidatat dolore non nisi nisi.",
                     projectImage: "/images/testing.png"
                 },
                 {
                     projectTitle: "China Mobile International (CMI)",
-                    projectLink: "www.youtube.com",
+                    projectLink: "http://www.youtube.com",
                     projectDesc: "Ad cillum cupidatat dolore non nisi nisi.",
                     projectImage: "/images/testing.png"
                 },
@@ -44,14 +44,62 @@ class Projects extends Component {
 
     }
 
+    renderProjectList = () => {
+        if(this.state.projects.length){
+
+            var html = [];
+            var isOdd = false;
+
+            if(this.state.projects.length % 2 === 1){
+                isOdd = true
+            }
+
+            for(var i = 0; i < this.state.projects.length; i++){
+                var className = ''
+                if(isOdd && i === this.state.projects.length - 1){
+                    className = 'full-width';
+                }
+                else if(i%4 === 0 || i%4 === 3){
+                    className = 'width-60';
+                }
+                else{
+                    className = 'width-40';
+                }
+                html.push(
+                    <Project
+                        key={i}
+                        projectClassName={className}
+                        projectTitle={this.state.projects[i].projectTitle} 
+                        projectLink={this.state.projects[i].projectLink}
+                        projectDesc={this.state.projects[i].projectDesc}
+                        projectImage={this.state.projects[i].projectImage}
+                    >
+                    </Project>
+                )
+            }
+
+            return (
+                <div>
+                    {html}
+                </div>
+            )
+        }
+        else{
+            return (
+                <div>No projects</div>
+            )
+        }
+    }
+
 
     render(){
+
         return (
             <div className="section projects">
                 <span className="title">Projects</span>
 
                 {/* Project list */}
-                <div className="list">
+                {/* <div className="list">
                     {this.state.projects.map((project, index) => {
                         return (
                             <Project 
@@ -64,7 +112,9 @@ class Projects extends Component {
                             </Project>
                         )
                     })}
-                </div>
+                </div> */}
+
+                {this.renderProjectList()}
 
             </div>
         );
